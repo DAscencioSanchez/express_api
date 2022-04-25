488e3a604143ce7d9a874ffb78aff292c400c393
DAscencioSanchez <diaspm77@gmail.com>
2022-04-25 11:23:54 -0500

    Step 6: adding GET BY ID on /v1/explorers/:id

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ app.use(express.json()) // indicamos que usaremos JSON
 //Puerto en que podremos ver nuestra app: localhost:3000
 const port = 3000
 
+//Esto es un ENDPOINT
 //routes
 //Methods HTTP; GET, POST, PUT, DELETE,
 app.get('/v1/explorers', (req,res) => {
@@ -23,4 +24,11 @@ app.get('/v1/explorers', (req,res) => {
 //Con esto inicializamos esta app
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
+})
+
+app.get('/v1/explorers/:id', (req,res) => {
+    console.log(`GET BY ID Explorers V1 API ${new Date()}`)
+    const explorer1 = {id:1, name: "Explorer1"}
+    //HTTP code status: 200
+    res.status(200).json(explorer1)
 })
